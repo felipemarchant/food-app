71cfcbcb52df377313a6be034d7fa8f419210d91
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-05-12 22:24:50 -0300

    Searchbar e CardFood

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foodapp/src/widget/bought_food.dart';
 import 'package:foodapp/src/widget/food_category.dart';
 import 'package:foodapp/src/widget/home_top_info.dart';
+import 'package:foodapp/src/widget/search_field.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,8 +18,29 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           HomeTopInfo(),
           FoodCategory(),
+          SizedBox(height: 20.0,),
+          SearchField(),
+          SizedBox(height: 20.0,),
+          FoodSection(),
+          SizedBox(height: 20.0,),
+          Container(child: BoughtFoods(),)
         ],
       ),
+    );
+  }
+
+  Widget FoodSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text("Frequently Bought Foods",
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+        GestureDetector(
+          onTap: () {},
+          child: Text("View All",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.orangeAccent)),
+        ),
+      ],
     );
   }
 }
